7e95c23d8e606482710ed09e840c626f0708c7b1
Santosh Karna <santoshkarna@gmail.com>
2025-03-26 06:38:16 +0545

    added few code

--- src/day1/basic/HelloWorld.java
+++ src/day1/basic/HelloWorld.java
@@ -3,5 +3,6 @@ package day1.basic;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Welcome to Java World!!!");
+        System.out.println("Hello, I am learning git too.");
     }
 }
