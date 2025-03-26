1271ddf88e5af63aa90f9ff80d3e7f3f934db6ab
Santosh Lal Karna <santoshkarna@gmail.com>
2025-03-26 06:55:04 +0545

    Update Introduction.java

--- src/day1/basic/Introduction.java
+++ src/day1/basic/Introduction.java
@@ -8,7 +8,7 @@ public class Introduction {
         System.out.println("I am Java Developer.");
         System.out.println("I also teach Java part time.");
         System.out.println("Hello, good morning from Nepal.");
-
+        System.out.println("Hello, good morning from Nepal.");
         System.out.println("Hello, I will teach you java for 2 months.\nAfter 2 months, \nyou will become Java Developer.");
     }
 }
